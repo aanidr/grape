32c3cebab49a8878c1e27d61364235b25ee9fed9
Adrian Nawrocki <adrian.nawrocki@netguru.co>
2019-03-04 06:10:29 +0100

    full api

--- app/api/factory/v1/products.rb
+++ app/api/factory/v1/products.rb
@@ -11,11 +11,39 @@ module Factory
           present products, with: Factory::Entities::Product
         end
 
-        desc 'Return single product'
+        desc 'Create new product'
+        params do
+          requires :code, type: String
+          requires :price, type: Integer
+          requires :production_time, type: Integer
+        end
+        post do
+          Product.create(
+            code: params[:code],
+            price: params[:price],
+            production_time: params[:production_time]
+          )
+        end
+
         route_param :id do
+          desc 'Return single product'
           get do
             product = Product.find(params[:id])
             present product, with: Factory::Entities::Product
+          end
+
+          desc 'Update single product data'
+          put do
+            Product.find(params[:id])
+              .update_attributes(
+                price: params[:price],
+                production_time: params[:production_time]
+              )
+          end
+
+          desc 'Remove single product'
+          delete do
+            Product.find(params[:id]).destroy
           end
         end
       end
